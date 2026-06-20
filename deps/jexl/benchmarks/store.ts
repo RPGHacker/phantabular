de0e36f07ae4ed1ed1d3a48c42fa4c14b215f3a4
RPG Hacker <markus_wall@web.de>
2026-06-20 23:22:42 +0200

    Removed Jexl

--- deps/jexl/benchmarks/store.ts
+++
@@ -1,4 +0,0 @@
-import { FileStore } from '@pawel-up/benchmark'
-export type BenchTypes = 'expressions' | 'validation'
-const store = new FileStore<BenchTypes>({ basePath: './benchmarks/history' })
-export { store }
